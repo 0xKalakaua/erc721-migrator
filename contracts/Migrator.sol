028d0ecbdcce4ab61a447c4ca71968d6bdbc694d
thomas <thomashm88@gmail.com>
2021-10-04 18:03:12 +0200

    Remove unnecesary import

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-//import "../../interfaces/ERC721Interface.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
 contract Migrator is Context, IERC721Receiver {
@@ -16,9 +15,11 @@ contract Migrator is Context, IERC721Receiver {
 
 
     constructor (address minter, address oldContract, address newContract) {
-        oldToNewId[3] = 1;
-        oldToNewId[2] = 3;
-        oldToNewId[1] = 2;
+        oldToNewId[2] = 1;
+        oldToNewId[4] = 2;
+        oldToNewId[6] = 3;
+        oldToNewId[8] = 4;
+        oldToNewId[10] = 5;
         _oldContract = oldContract;
         _newContract = newContract;
         petZoo = IERC721(_oldContract);
