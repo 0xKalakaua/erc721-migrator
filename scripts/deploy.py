63a4b1105aebfba1dc8876d7ae450e7723df114a
0xKalakaua <0xKalakaua@gmail.com>
2021-11-02 14:11:29 +0100

    Set up deploy script ready for mainnet launch

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,10 +4,10 @@ from brownie import Migrator, accounts, network, config
 
 
 def main():
-    minter = accounts.load("dev")
-    admin = accounts.load("admin")
+    dev = accounts.load("dev")
+    minter = "<Tombheads address>"
     print(network.show_active())
     publish_source = True # Not supported on Testnet
-    old_contract = "0x99ed0fD437d663f54046150E621629409A882901" 
-    new_contract = "0xE36a7641eEb756b16ad6BFB177955A2d13bd0c87"
-    Migrator.deploy(minter, old_contract, new_contract, {"from": admin}, publish_source=publish_source)
+    old_contract = "<old contract>" 
+    new_contract = "<new contract>"
+    Migrator.deploy(minter, old_contract, new_contract, {"from": dev}, publish_source=publish_source)
