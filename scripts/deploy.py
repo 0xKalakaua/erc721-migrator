50183ab5b9da9978fca38532a8fa782e998c52eb
0xKalakaua <0xKalakaua@gmail.com>
2021-12-09 17:34:28 -0600

    Get everything ready for Zvonarka deployment

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,9 +5,9 @@ from brownie import Migrator, accounts, network, config
 
 def main():
     dev = accounts.load("dev")
-    minter = "<Tombheads address>"
+    minter = "0x3B6B88535C0fa08179262829558F49C72EE6c1a0"
     print(network.show_active())
-    publish_source = True # Not supported on Testnet
-    old_contract = "<old contract>" 
-    new_contract = "<new contract>"
+    publish_source = True
+    old_contract = "0xd606543c1c7607bf02B9536e9B31cdc1CF564c1E" 
+    new_contract = "0x24487f31B75c2B740C82f64b9e94477Ed12757e7"
     Migrator.deploy(minter, old_contract, new_contract, {"from": dev}, publish_source=publish_source)
