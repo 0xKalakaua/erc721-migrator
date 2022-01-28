b63503a89ee545b7754d07b0d51d15da31b693df
0xKalakaua <0xKalakaua@gmail.com>
2022-01-28 14:47:56 +0100

    Fantolanterns Customs Migrator ready

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,9 +5,9 @@ from brownie import Migrator, accounts, network, config
 
 def main():
     dev = accounts.load("dev")
-    minter = "0x0A75029385FDF5e50e8753234Df0346784eCbdC1"
+    minter = "0x4E5710a75c1c718Cf7F79688883F94Ea6cA02043"
     print(network.show_active())
     publish_source = True
     old_contract = "0xd606543c1c7607bf02B9536e9B31cdc1CF564c1E" 
-    new_contract = "0x903efDA32f6d85ae074c1948C8d6B54F2421949f"
+    new_contract = "0x7Eb605E32AA833dff55DF3BAe51Afb82eC61e372"
     Migrator.deploy(minter, old_contract, new_contract, {"from": dev}, publish_source=publish_source)
